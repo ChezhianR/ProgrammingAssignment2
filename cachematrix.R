0e4fbbe6714fcf5a82d5e78a09818f3ab1a8c48c
ChezhianR <androidgames6786@gmail.com>
2017-09-04 06:01:57 -0700

    Intial Check in

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,54 @@
 ## functions do
 
 ## Write a short comment describing this function
+##  
+## returns a matrix and its inverse as List
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Initialize Inverse Matrix to NULL
+  inv <- NULL
+  #Set Matrix and inverse
+  setfn <- function(y)
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  #Get value of  Matrix
+  getfn <- function() 
+  {
+    x
+  }
+  
+  # set Inverse of Matrix
+  setInversefn <- function(inverse)
+  {
+    inv <<- inverse 
+  }
+  # Get value of Inverse Matrix
+  getInversefn <- function()
+  {
+    inv			
+  }
+  #returns the list of values
+  list(set = setfn,
+       get = getfn,
+       setInverse = setInversefn,
+       getInverse = getInversefn)
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this 
+##Checks cache for existing value, if not present it computes the inverse of the matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
